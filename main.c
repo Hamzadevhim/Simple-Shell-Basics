24951e8e9d4fd28245e7c5e74334b308d88f518c
Hamza Al Rufaiee <Hamza.alrufaiee@gmail.com>
2026-01-12 15:53:08 -0600

    new tutorial

--- main.c
+++ main.c
@@ -1,30 +1,6 @@
-#include <stdio.h>
+#include <func.h>
 
-//loop
-//parsing
-//command doing
-
-void shell_loop(char**, env)
+int main(void)
 {
-    char* input = NULL;
-    size_t input_size = 0;
-
-    char** args;
-
-    while (true)
-    {
-        getline(&input, &input_size, stdin);
-
-        printf("input: %s", input);
-    }
-}
-
-int main(int argc, char** argv, char** env)
-{
-    (void)argc;
-    (void)argv;
-
-    shell_loop(env);
-
     return 0;
 }
